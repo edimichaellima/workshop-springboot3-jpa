513ad09fb201ef62a0f14920f92a70666681d948
Edimichael Lima <edimichael.lima@gmail.com>
2023-06-09 01:48:35 -0300

    Project created

---
+++ src/main/java/com/assum/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.assum.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
