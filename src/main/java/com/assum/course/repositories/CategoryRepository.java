77ea5b8eae2e3302f2e95f1642cef851e563a539
Edimichael Lima <edimichael.lima@gmail.com>
2023-06-10 11:43:11 -0300

    Category

---
+++ src/main/java/com/assum/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.assum.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.assum.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
